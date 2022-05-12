257bc13f9559671bad7aacefe757a0010f087bc9
DeveloperMahmud <mrepte700@gmail.com>
2022-05-12 21:50:17 +0600

    add funtion to convert lenght  inch to feet

--- inchToFeet.js
+++ inchToFeet.js
@@ -1,4 +1,20 @@
-var inch = 100;
-// var feet = inch/12;
+// basic convertion code
 
-console.log(inch);
+// var inch = 100;
+// var feet = inch/12;
+// console.log(feet);
+
+// Functional Convertion code
+
+function inchTofeet(inch){
+    var feet = inch/12;
+    return feet;
+}
+var top = inchTofeet(20);
+var left = inchTofeet(200);
+var right = inchTofeet(2066);
+var bottom = inchTofeet(2875);
+console.log(top);
+console.log(bottom);
+console.log(left);
+console.log(right);
